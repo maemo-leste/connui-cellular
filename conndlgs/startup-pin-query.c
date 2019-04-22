b3af937986aa06c469414551e4f7d14ea3d3bc68
Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>
2019-04-22 23:20:17 +0300

    add startup-pin-query
    Signed-off-by: Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>

---
+++ conndlgs/startup-pin-query.c
@@ -0,0 +1,67 @@
+#include <libosso.h>
+#include <hildon/hildon.h>
+#include <dbus/dbus-glib.h>
+#include <connui/connui-flightmode.h>
+#include <conbtui/gateway/common.h>
+
+#include <libintl.h>
+
+#include "connui-cellular.h"
+
+#include "config.h"
+
+#define _(msgid) dgettext(GETTEXT_PACKAGE, msgid)
+
+static gboolean exit_flightmode ;
+
+static gboolean
+query_pin_cb(gpointer user_data)
+{
+  if (connui_cell_code_ui_init(NULL, TRUE))
+  {
+    connui_cell_code_ui_is_sim_locked_with_error();
+    connui_cell_code_ui_destroy();
+  }
+  else
+    g_warning("Unable to initialize SIM");
+
+  if (exit_flightmode)
+  {
+    if (!connui_flightmode_off())
+      g_warning("Unable to turn flightmode off");
+  }
+
+  gtk_main_quit();
+
+  return FALSE;
+}
+
+static void
+flightmode_status_cb(dbus_bool_t offline, gpointer user_data)
+{
+  if (offline)
+  {
+    if (!gateway_common_show_flight_mode(NULL))
+      gtk_main_quit();
+
+    exit_flightmode = TRUE;
+  }
+
+  g_idle_add(query_pin_cb, NULL);
+}
+
+int
+main(int argc, char **argv)
+{
+  setlocale(LC_ALL, "");
+  bindtextdomain("osso-connectivity-ui", "/usr/share/locale");
+  textdomain("osso-connectivity-ui");
+  hildon_gtk_init(&argc, &argv);
+  dbus_g_thread_init();
+
+  if (!connui_flightmode_status(flightmode_status_cb, NULL))
+    g_warning("Unable to register flightmode status!");
+  gtk_main();
+
+  return 0;
+}
